c6a1e473def5430c2b4ed690906dd083004061fb
Ivshin Aleksei <ivshinalekseim@mail.ru>
2014-10-07 16:29:44 +0400

    Mini fix

--- src/main/java/client/SimpleClient.java
+++ src/main/java/client/SimpleClient.java
@@ -14,6 +14,7 @@ import webservice.Exception_Exception;
 import webservice.MerchantDomain;
 import webservice.SalesDomain;
 import webservice.ShowService;
+import webservice.StoreDomain;
 
 public class SimpleClient {
 	static final Logger logger = LoggerFactory.getLogger(SimpleClient.class);
@@ -27,14 +28,30 @@ public class SimpleClient {
 		servicePort = service.getPort(ShowService.class);
 	}
 
+	/**
+	 * Get all mark names.
+	 * @return mark names
+	 */
 	public List<String> getMarks() {
 		return servicePort.getCarMarkList();
 	}
 
+	/**
+	 * Get all merchants.
+	 * @return merchants
+	 */
 	public List<MerchantDomain> getMerchants() {
 		return servicePort.getAllMerchants();
 	}
 
+	/**
+	 * Sale car.
+	 * @param customer buy car
+	 * @param merchant sell car
+	 * @param car car
+	 * @return new sale object
+	 * @throws Exception
+	 */
 	public SalesDomain saleCar(CustomerDomain customer,
 			MerchantDomain merchant, CarDomain car) throws Exception {
 		try {
@@ -45,27 +62,65 @@ public class SimpleClient {
 		}
 	}
 
+	/**
+	 * Get cars by mark.
+	 * @param markName mark name
+	 * @return
+	 */
 	public List<CarDomain> getCarsByMark(String markName){
 		return servicePort.getCarByMark(markName);
 	}
 	
+	/**
+	 * Get cars by mark and model names.
+	 * @param mark mark name
+	 * @param model model name
+	 * @return
+	 */
 	public List<CarDomain> getCarsByMarkAndModel(String mark, String model){
 		return servicePort.getCarByMarkAndModel(mark, model);
 	}
 	
+	/**
+	 * Get car by id.
+	 * @param id car id
+	 * @return
+	 */
 	public CarDomain getCarById(long id){
 		return servicePort.getCarById(id);
 	}
 	
+	/**
+	 * Get merchant by id.
+	 * @param id merchant id
+	 * @return
+	 */
 	public MerchantDomain getMerchantById(int id){
 		return servicePort.getMerchantById(id);
 	}
 	
+	/**
+	 * Find one car by mark name , model name and modification.
+	 * @param markName mark name
+	 * @param modelName model name
+	 * @param modificationName modification
+	 * @return car
+	 * @throws Exception
+	 */
 	public CarDomain findCar(String markName, String modelName, String modificationName) throws Exception{
 		try {
 			return servicePort.findOneCar(markName, modelName, modificationName);
 		} catch (Exception_Exception e) {
 			throw new Exception(e);
 		}
+	}
+	
+	/**
+	 * Get store item of car.
+	 * @param car car
+	 * @return
+	 */
+	public StoreDomain getStoreByCar(CarDomain car){
+		return servicePort.getStore(car);
 	}
 }
