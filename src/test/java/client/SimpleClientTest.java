8d5ce392b60d3a6475fd5e62f419efc839a1b1e0
Ivshin Aleksei <ivshinalekseim@mail.ru>
2014-10-09 15:41:22 +0400

    Add test for clients

--- src/test/java/client/SimpleClientTest.java
+++ src/test/java/client/SimpleClientTest.java
@@ -11,6 +11,7 @@ import webservice.CarElement;
 import webservice.CustomerElement;
 import webservice.MerchantElement;
 import webservice.SalesElement;
+import webservice.StoreElement;
 
 public class SimpleClientTest {
 
@@ -34,7 +35,7 @@ public class SimpleClientTest {
 	}
 	
 	@Test
-	public void testSealeCare(){
+	public void testSeleCare(){
 		CarElement car = client.getCarById(2);
 		MerchantElement merchant = client.getMerchantById(2);
 		CustomerElement  customerElement = client.findCustomerByPassport("9100", "100100");
@@ -48,6 +49,53 @@ public class SimpleClientTest {
 			return;
 		}
 		assertNotNull(salesElement);
+	}
+	
+	@Test
+	public void testGetCarByMark(){
+		List<CarElement> cars = client.getCarsByMark("Audi");
+		assertNotNull(cars);
+	}
+	
+
+	@Test
+	public void testGetCarByMarkAnModel(){
+		List<CarElement> cars = client.getCarsByMarkAndModel("Audi","R8");
+		assertNotNull(cars);
+	}
+	
+	@Test
+	public void testGetCarAndGetStore(){
+		CarElement car;
+		try {
+			car = client.findCar("Audi", "R8", "6.2 MT (442 hs)");
+		} catch (Exception e) {
+			fail(e.getMessage());
+			return;
+		}
+		assertNotNull(car);
+		StoreElement store = client.getStoreByCar(car);
+		assertNotNull(store);
+		assertEquals(store.getCar().getId(), car.getId());
+		assertEquals(store.getCar().getMark(), car.getMark());
+		assertEquals(store.getCar().getModel(), car.getModel());
+		assertEquals(store.getCar().getModification(), car.getModification());
+		assertEquals(store.getQuantity(), 2);		
+	}
+	
+	@Test
+	public void testGetAllStroreItems(){
+		List<StoreElement> storeItems = client.getAllStores();
+		assertNotNull(storeItems);
+		assertFalse(storeItems.isEmpty());
+	}
+	
+
+	@Test
+	public void testGetAllSaleItems(){
+		List<SalesElement> salesElements = client.getAllSales();
+		assertNotNull(salesElements);
+		assertFalse(salesElements.isEmpty());
 	}
 
 }
